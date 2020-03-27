b39aeab4b0d92b33cd68e899c02cdde38de91b6a
charlesladzro <29816789+charlesladzro@users.noreply.github.com>
2020-03-27 13:49:15 +0100

    Sauv 27/03/2020

--- MUX2.vhd
+++ MUX2.vhd
@@ -2,20 +2,21 @@ library IEEE;	-- type boolean, bit, character, severity_level
 use IEEE.STD_LOGIC_1164.ALL;	-- definit les types std_logic , std_logic_vector , std_ulogic, les opÃ©rateurs logiques et les fonctions de recherches de fronts rising_edge() et falling_edge(). introduit par IEE 1164 en 1992.
 use IEEE.NUMERIC_STD.ALL;		-- definit les sous-types entier "signed" ou "unsigned", introduit par IEEE 1076.3
 
-entity Registre_tb is 
-
+entity MUX2 is 
+	A,B : in std_logic_vector (N-1 downto 0);
+	COM : in std_logic;
+	S : out std_logic_vector (N-1 downto 1);
 end entity;
 
-architecture arc of Registre_tb is
-	--Definitions des variables d'entrees et sorties
-		signal CLK, We, Rst :  std_logic;
-		signal W :  std_logic_vector(31 downto 0);
-		signal Ra, Rb, Rw :  std_logic_vector(3 downto 0);
-		signal A , B :  std_logic_vector (31 downto 0); 
+architecture arc of MUX2 is
+		--Definitions des variables d'entrees et sorties
+		
+		
 	begin
-		uut: entity work.Registre port map(CLK,We,Rst,W,Ra,Rb,Rw,A,B);
-		process
-			begin
-			-- A continuer 
-		end process;
+	
+	with COM select
+		S <= A when '0',
+			 B when others;
+			  
+
 	end arc;
---
+++ projet arsy/unite_gestion_instr.vhd
@@ -0,0 +1,28 @@
+library IEEE;	-- type boolean, bit, character, severity_level
+use IEEE.STD_LOGIC_1164.ALL;	-- definit les types std_logic , std_logic_vector , std_ulogic, les opÃ©rateurs logiques et les fonctions de recherches de fronts rising_edge() et falling_edge(). introduit par IEE 1164 en 1992.
+use IEEE.NUMERIC_STD.ALL;		-- definit les sous-types entier "signed" ou "unsigned", introduit par IEEE 1076.3
+
+entity unite_gestion_instr is 
+port(
+	--Definitions des variables d'entrees et sorties
+		CLK, Rst, nPCsel : in std_logic;
+		Offset : in std_logic_vector(24 downto 0);
+		Instruction : out std_logic_vector (31 downto 0)
+		
+		);
+end entity;
+
+architecture arc of unite_gestion_instr is
+	
+	signal Addr : std_logic_vector (31 downto 0);
+	signal S_Alu1, S_Alu_Offset, S_Offset, S_MUX:  std_logic_vector(31 downto 0);
+	signal n_Alu1, n_Alu_Offset: std_logic;
+
+	begin
+		uut_PC: entity work.PC(arc) port map(CLK,Rst,S_MUX,Addr);
+		uut_ALU_Plus1: entity work.ALU(arc) port map(Addr,X"00000001","00",S_Alu1,n_Alu1);
+		uut_MUX: entity work.MUX2(arc) port map(S_Alu1,S_Alu_Offset,nPCsel,S_MUX);
+		uut_memoire_instr: entity work.memoire_instr(arc) port map(Instruction, Addr);
+		uut_ALU_Plus1_PlusOffset: entity work.ALU(arc) port map(S_Offset,S_Alu1,"00",S_Alu_Offset,n_Alu_Offset);
+		uut_extention: entity work.extention(arc) generic map(24) port map(Offset,S_Offset);
+end arc;
